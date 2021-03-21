97e8e33121d7f2ca5c20399b2379aeb71928861f
KofoworolaOgunleye <kay16th@yahoo.com>
2021-03-21 01:28:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -326,7 +326,7 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
 
 resource "aws_autoscaling_policy" "scale_down_policy" {
   name                   = "scale_down_policy"
-  scaling_adjustment     = -1
+  scaling_adjustment     = -2
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.WebserverASG.name
