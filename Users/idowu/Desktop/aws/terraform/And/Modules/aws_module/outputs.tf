ccd012535349aa72b535026f9a57490c9a9b540b
KofoworolaOgunleye <kay16th@yahoo.com>
2021-05-19 12:19:19 +0100

    Delete Users/idowu directory

--- Users/idowu/Desktop/aws/terraform/And/Modules/aws_module/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "elb_dns_name" {
-  value       = aws_lb.WebserverELB.dns_name
-  description = "load balancer dns"
-}
-
-output "acm_certificate_arn" {
-  value       = aws_acm_certificate.skyetag.arn
-  description = "arn of acm certificate"
-} 
